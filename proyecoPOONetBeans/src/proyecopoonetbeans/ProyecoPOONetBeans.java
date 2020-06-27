a6cbfd1a8e98bfa81a59f13a1f02ee15751a5443
Jazzeauz <jochoar1500@alumno.ipn.mx>
2020-06-27 15:25:26 -0500

    Creacion del proyecto

---
+++ proyecoPOONetBeans/src/proyecopoonetbeans/ProyecoPOONetBeans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecopoonetbeans;
+
+/**
+ *
+ * @author Jahaziel
+ */
+public class ProyecoPOONetBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+        // TODO code application logic here
+    }
+    
+}
