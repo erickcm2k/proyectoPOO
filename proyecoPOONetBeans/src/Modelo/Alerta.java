183f2df35ca692a3eb4b4e939b423f1442be60fe
Jazzeauz <jochoar1500@alumno.ipn.mx>
2020-06-30 16:53:49 -0500

    Secreo clase Alerta

---
+++ proyecoPOONetBeans/src/Modelo/Alerta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Jahaziel
+ */
+public class Alerta {
+    //Atributos Alerta
+    private String mes;
+    private int numAdeudos;
+    
+    //Metodo que notifica al usuario que tiene adeudos
+    public Usuario notificarAdeudos(){
+        //Modifcar
+        return null;
+    }
+    
+}
